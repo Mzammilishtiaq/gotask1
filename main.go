c373222dae5ef58f7de16f794a38ff837e317478
Mzammilishtiaq <tomuzammilishtiaq@gmail.com>
2025-04-11 16:50:34 +0500

    combine function

--- main.go
+++ main.go
@@ -7,112 +7,6 @@ import (
 	"time"
 )
 
-// WordCount counts the number of words in a string.
-// func WordCount(s string, ch chan int) {
-// 	word := 0
-// 	for i := 0; i < len(s); i++ {
-// 		if s[i] == ' ' {
-// 			word++
-// 		}
-// 	}
-// 	ch <- word
-// }
-
-// func LineCount(s string, ch chan int) {
-// 	word := 0
-// 	for i := 0; i < len(s); i++ {
-// 		if s[i] == '\n' {
-// 			word++
-// 		}
-// 	}
-// 	ch <- word
-// }
-
-// func PunctcationCount(s string, ch chan int) {
-// 	word := 0
-// 	for i := 0; i < len(s); i++ {
-// 		if s[i] == '.' || s[i] == '?' || s[i] == '!' || s[i] == ',' ||
-// 			s[i] == ';' ||
-// 			s[i] == ':' || s[i] == '"' ||
-// 			s[i] == '\'' || s[i] == '(' || s[i] == ')' ||
-// 			s[i] == '[' || s[i] == ']' || s[i] == '{' ||
-// 			s[i] == '}' || s[i] == '<' || s[i] == '>' ||
-// 			s[i] == '/' || s[i] == '\\' || s[i] == '^' ||
-// 			s[i] == '_' || s[i] == '=' || s[i] == '|' ||
-// 			s[i] == '`' || s[i] == '~' {
-// 			word++
-// 		}
-// 	}
-// 	ch <- word
-// }
-
-// func VowelCount(s string, ch chan int) {
-// 	word := 0
-// 	for i := 0; i < len(s); i++ {
-// 		if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
-// 			s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||
-// 			s[i] == 'E' || s[i] == 'I' || s[i] == 'O' ||
-// 			s[i] == 'U' {
-// 			word++
-// 		}
-// 	}
-// 	ch <- word
-// }
-
-// func ParaCount(s string, ch chan int) {
-// 	word := 0
-// 	for i := 0; i < len(s); i++ {
-// 		if s[i] == '\n' || s[i] == '\t' || s[i] == '\r' || s[i] == '\f' || s[i] == ' ' {
-// 			word++
-// 		}
-// 	}
-// 	ch <- word
-// }
-
-// func SpecialCharacterCount(s string, ch chan int) {
-// 	word := 0
-// 	for i := 0; i < len(s); i++ {
-// 		if s[i] == '@' || s[i] == '#' ||
-// 			s[i] == '$' || s[i] == '%' || s[i] == '^' ||
-// 			s[i] == '&' || s[i] == '*' || s[i] == '+' ||
-// 			s[i] == '-' {
-// 			word++
-// 		}
-// 	}
-// 	ch <- word
-// }
-
-// func DigitsCount(s string, ch chan int) {
-// 	word := 0
-// 	for i := 0; i < len(s); i++ {
-// 		if s[i] == '0' || s[i] == '1' || s[i] == '2' ||
-// 			s[i] == '3' || s[i] == '4' || s[i] == '5' ||
-// 			s[i] == '6' || s[i] == '7' || s[i] == '8' ||
-// 			s[i] == '9' {
-// 			word++
-// 		}
-// 	}
-// 	ch <- word
-// }
-
-func ConsientCount(s string, ch chan int) {
-	word := 0
-	for i := 0; i < len(s); i++ {
-		if s[i] == 'b' || s[i] == 'c' ||
-			s[i] == 'd' || s[i] == 'f' ||
-			s[i] == 'g' || s[i] == 'h' ||
-			s[i] == 'j' || s[i] == 'k' ||
-			s[i] == 'l' || s[i] == 'm' ||
-			s[i] == 'n' || s[i] == 'p' ||
-			s[i] == 'q' || s[i] == 'r' ||
-			s[i] == 's' || s[i] == 'v' ||
-			s[i] == 'w' || s[i] == 'x' ||
-			s[i] == 'y' || s[i] == 'z' {
-			word++
-		}
-	}
-	ch <- word
-}
 
 func combinefunction(s string) (int, int, int, int, int, int, int, int) {
 	words := 0
@@ -165,7 +59,7 @@ func combinefunction(s string) (int, int, int, int, int, int, int, int) {
 	return words, line, spaces, punctcation, vowels, digits, ConsientCount, SpecialCharacter
 }
 
-func main() {
+func mainTask1() {
 	combinestarttime := time.Now()
 	filename, err := os.ReadFile("text_file.txt")
 	if err != nil {
@@ -221,73 +115,4 @@ func main() {
 	combinrendtime := time.Since(combinestarttime)
 	fmt.Printf("Total Combine function time : %s", combinrendtime)
 
-	// channel create in word count
-	// wordCountChannel := make(chan int)
-	// go WordCount(str, wordCountChannel)
-
-	// channel create in line count
-	// LinecountChannel := make(chan int)
-	// go LineCount(str, LinecountChannel)
-
-	// channel create in punctcation count
-	// PunctcationCountChannel := make(chan int)
-	// go PunctcationCount(str, PunctcationCountChannel)
-
-	// channel create in vowel count
-	// totalvowelChannel := make(chan int)
-	// go VowelCount(str, totalvowelChannel)
-
-	// channel create in vowel count
-	// totalparaChannel := make(chan int)
-	// go ParaCount(str, totalparaChannel)
-
-	// channel create in vowel count
-	// totalSpecialCharacterChannel := make(chan int)
-	// go SpecialCharacterCount(str, totalSpecialCharacterChannel)
-
-	// channel create in digit count
-	// digitcontchannel := make(chan int)
-	// go DigitsCount(str, digitcontchannel)
-
-	//channel create in combine function
-	// totalcombinechannel := make(chan int)
-	// go combinefunction(str, totalcombinechannel)
-
-	// goroutines
-	// totalWords := <-wordCountChannel
-	// totallines := <-LinecountChannel
-	// TotalPunctcationCount := <-PunctcationCountChannel
-	// totalVowel := <-totalvowelChannel
-	// totalParagraph := <-totalparaChannel
-	// totalSpecialCharacterCount := <-totalSpecialCharacterChannel
-	// totalDigitCount := <-digitcontchannel
-
-	// fmt.Println("<====================== start all seperate function run============================>")
-	// fmt.Println("Total words in file:", totalWords)
-	// fmt.Println("Total line in file:", totallines)
-	// fmt.Println("Total PunctcationCount in file:", TotalPunctcationCount)
-	// fmt.Println("Total VowelCount in file:", totalVowel)
-	// fmt.Println("Total ParagraphCount in file:", totalParagraph)
-	// fmt.Println("Total SpecialCharacterCount in file:", totalSpecialCharacterCount)
-	// fmt.Println("Total DigitCount in file:", totalDigitCount)
-	// speratefunctiontime := time.Since(start)
-	// fmt.Printf("Total Sperate function time :%s", speratefunctiontime)
-	// fmt.Println("\n<====================== end all seperate function run============================>")
-	// totalCombinefunction:= <-totalcombinechannel
-
-	// combinestarttime := time.Now()
-	// words, lines, spaces, punctuations, vowels, digits, consonants, specialCharacters := combinefunction(str)
-	// fmt.Println("\n<====================== start combine function run============================>")
-	// fmt.Println("Combine Total words in file:", words)
-	// fmt.Println("Combine Total line in file:", lines)
-	// fmt.Println("Combine Total space in file:", spaces)
-	// fmt.Println("Combine Total vowels in file:", vowels)
-	// fmt.Println("Combine Total punctuations in file:", punctuations)
-	// fmt.Println("Combine Total digits in file:", digits)
-	// fmt.Println("Combine Total consonants in file:", consonants)
-	// fmt.Println("Combine Total specialsCharacters in file:", specialCharacters)
-	// combinrendtime := time.Since(combinestarttime)
-	// fmt.Printf("Total Combine function time : %s", combinrendtime)
-	// fmt.Println("\n<======================end combine function run============================>")
-
-}
+	}
